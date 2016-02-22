2dc9dbbb45208e94a54ce249d30da6b4843d9452
mgaillard <gaillard.mathieu.39@gmail.com>
2016-02-22 13:07:16 +0100

    Convert indentations to spaces.

--- response.cpp
+++ response.cpp
@@ -10,48 +10,48 @@ Response::Response(ostream& output_stream) : output_stream_(output_stream), stat
 
 void Response::SetStatusCode(int status_code, const string& status_text)
 {
-	status_code_ = status_code;
+    status_code_ = status_code;
 
-	if (!status_text.empty())
-	{
-		status_text_ = status_text;
-	}
-	else
-	{
-		//If the status_text is empty, it will be automatically populated for the known status code.
-		status_text_ = "Default status text";
-	}
+    if (!status_text.empty())
+    {
+        status_text_ = status_text;
+    }
+    else
+    {
+        //If the status_text is empty, it will be automatically populated for the known status code.
+        status_text_ = "Default status text";
+    }
 }
 
 void Response::SetHeader(const string& name, const string& value)
 {
-	map<string, string>::iterator iterator = headers_.find(name);
-	//If the key already exists.
-	if (iterator != headers_.end())
-	{
-		//Replace the value.
-		iterator->second = value;
-	}
-	else
-	{
-		//Insert a new header.
-		headers_.insert(make_pair(name, value));
-	}
+    map<string, string>::iterator iterator = headers_.find(name);
+    //If the key already exists.
+    if (iterator != headers_.end())
+    {
+        //Replace the value.
+        iterator->second = value;
+    }
+    else
+    {
+        //Insert a new header.
+        headers_.insert(make_pair(name, value));
+    }
 }
 
 void Response::SetContent(const string& content)
 {
-	content_ = content;
+    content_ = content;
 }
 
 void Response::Send()
 {
-	output_stream_ << "Status: " << status_code_ << " " << status_text_ << "\r\n";
-	//Headers
-	for (map<string, string>::const_iterator it_header = headers_.begin();it_header != headers_.end();++it_header)
-	{
-		output_stream_ << it_header->first << ": " << it_header->second << "\r\n";
-	}
-	output_stream_ <<  "\r\n";
-	output_stream_ << content_;
+    output_stream_ << "Status: " << status_code_ << " " << status_text_ << "\r\n";
+    //Headers
+    for (map<string, string>::const_iterator it_header = headers_.begin();it_header != headers_.end();++it_header)
+    {
+        output_stream_ << it_header->first << ": " << it_header->second << "\r\n";
+    }
+    output_stream_ <<  "\r\n";
+    output_stream_ << content_;
 }
--- response.h
+++ response.h
@@ -8,23 +8,23 @@ using namespace std;
 
 namespace NodeCpp 
 {
-	class Response
-	{
-	public:
-	    Response(ostream &output_stream);
-	    void SetStatusCode(int status_code, const string& status_text = "");
-	    void SetHeader(const string& name, const string& value);
-	    void SetContent(const string& content);
-	    void Send();
+    class Response
+    {
+    public:
+        Response(ostream &output_stream);
+        void SetStatusCode(int status_code, const string& status_text = "");
+        void SetHeader(const string& name, const string& value);
+        void SetContent(const string& content);
+        void Send();
 
-	private :
-		ostream& output_stream_;
+    private :
+        ostream& output_stream_;
 
-		int status_code_;
-		string status_text_;
-		map<string, string> headers_;
-		string content_;
-	};
+        int status_code_;
+        string status_text_;
+        map<string, string> headers_;
+        string content_;
+    };
 }
 
 #endif // RESPONSE_H
