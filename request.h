136d937cb71a34b4fa13dc291f17afae2e18d2ae
mgaillard <gaillard.mathieu.39@gmail.com>
2016-02-23 22:22:20 +0100

    Add getters to Request.

--- request.h
+++ request.h
@@ -14,6 +14,8 @@ namespace NodeCpp
     public:
         Request(FCGX_Request &request);
         string GetUri() const { return uri; }
+        string GetMethod() const { return method; }
+        string GetContent() const { return content; }
         string GetParameter(const string& name, const string& default_value) const;
         void SetParameters(const map<string, string>& parameters) { params = parameters; }
         void print_infos(ostream& stream) const;
